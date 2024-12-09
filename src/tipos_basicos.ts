4b34f69ac8db9416fcb57eb1513926059b664fff
Cesar Santos <csar.santos18@gmail.com>
2024-12-09 09:59:21 -0500

    tipos basicos typescript

---
+++ src/tipos_basicos.ts
@@ -0,0 +1,24 @@
+let estaChovendo: boolean = false; 
+
+estaChovendo = true;
+
+let idade: number = 28;
+
+let altura: number = 1.83;
+
+const nacionalidade: string = "brasileira";
+
+const colegas:string[] = ['Lucas', 'Fernanda', 'Cesar']
+const tecnologia:  Array<string> = ['html','css', 'js', 'typescript' ];
+const notas: ReadonlyArray<number> = [7,8,5,9]
+
+const lista: [nome :string, estarEstudando: boolean,idade: number] = ['cesar',true, 28]
+
+let idadeAna: number | string = 25
+idadeAna = "25 anos";
+
+let dadosDaApi: any;
+dadosDaApi =10
+dadosDaApi = true
+dadosDaApi = [1,2,3]
+dadosDaApi = 'string' 
